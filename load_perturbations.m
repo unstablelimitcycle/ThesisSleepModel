72f65534d74b5638956d3b82f7232dca91f5ca7b
unstablelimitcycle <dr.lisa.j.rogers@gmail.com>
2014-02-24 17:24:11 -0500

    Load Perturbations
    File to load perturbations

--- load_perturbations.m
+++ load_perturbations.m
@@ -1,1 +1,11 @@
+function V = load_perturbations
+
+A = load('long_night_times.txt');
+[m,n] = size(A);
+
+for i = 1:m
+    %vector of perturbation in units of var_gamma_sleep_model,i.e. 8
+    %minutes = 1 unit. Matrix A is the perturbations in minutes.
+    V(i) = A(i,1);
+end
 V = V/60;
